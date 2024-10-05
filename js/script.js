4e00b7260f69e3fa6757198cc6c07be2a1dd3629
AnantSagar <anantsagar0069@gmail.com>
2024-10-05 12:10:59 +0530

    fix repo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const btn = document.getElementById('button');
+const btn = document.getElementById('button'); //send message button
 document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
