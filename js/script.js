37507c906e096c51de855dba6b8a4e9ef03af01c
AnantSagar0069 <anantsagar0000@gmail.com>
2024-10-05 12:02:37 +0530

    fix the contact me section

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
-function sendEmail(event) {
-    event.preventDefault(); // Prevent the default form submission
+const btn = document.getElementById('button');
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
 
-    const serviceID = 'service_0q28umd'; // EmailJS service ID
-    const templateID = 'template_dps91zb'; // EmailJS template ID
+   btn.value = 'Sending...';
 
-    emailjs.sendForm(serviceID, templateID, this)
-        .then(() => {
-            alert('Message sent successfully!');
-            document.getElementById('contact-form').reset(); // Reset the form
-        }, (err) => {
-            alert('Failed to send the message. Please try again.');
-            console.error('Error:', err); // Log error to the console for debugging
-        });
-}
+   const serviceID = 'service_0q28umd';
+   const templateID = 'template_dps91zb';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Message sent successfully!!');
+      document.getElementById('form').reset();
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert('Failed to send the message. Please try again.');
+      alert(JSON.stringify(err));
+    });
+});
